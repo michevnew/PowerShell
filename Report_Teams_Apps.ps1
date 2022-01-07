52092a0048ec9156de81195db1d2b333ec584be6
Vasil Michev <38553333+michevnew@users.noreply.github.com>
2022-01-07 10:10:16 +0200

    Add files via upload

--- Report_Teams_Apps.ps1
+++ Report_Teams_Apps.ps1
@@ -1,16 +1,20 @@
-﻿$AppId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-$client_secret = Get-Content .\ReportingAPIsecret.txt | ConvertTo-SecureString
-$app_cred = New-Object System.Management.Automation.PsCredential($AppId, $client_secret)
-$TenantId = "tenant.onmicrosoft.com"
+﻿#Variables to configure
+$tenantID = "tenant.onmicrosoft.com" #your tenantID or tenant root domain
+$appID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" #the GUID of your app. For best result, use app with TeamsAppInstallation.ReadForTeam.All and TeamsTab.Read.All scopes granted.
+$client_secret = "verylongsecurestring" #client secret for the app
+
+#Prepare token request
+$url = 'https://login.microsoftonline.com/' + $tenantId + '/oauth2/v2.0/token'
 
 $body = @{
-    client_id     = $AppId
-    scope         = "https://graph.microsoft.com/.default"
-    client_secret = $app_cred.GetNetworkCredential().Password
-    grant_type    = "client_credentials"
+    grant_type = "client_credentials"
+    client_id = $appID
+    client_secret = $client_secret
+    scope = "https://graph.microsoft.com/.default"
 }
- 
-try { $tokenRequest = Invoke-WebRequest -Method Post -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing -ErrorAction Stop }
+
+#Obtain the token 
+try { $tokenRequest = Invoke-WebRequest -Method Post -Uri $url -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing -ErrorAction Stop }
 catch { Write-Host "Unable to obtain access token, aborting..."; return }
 
 $token = ($tokenRequest.Content | ConvertFrom-Json).access_token
@@ -20,8 +24,9 @@ $authHeader1 = @{
    'Authorization'="Bearer $token"
 }
 
+#Get a list of all Teams
 $Teams = @()
-$uri = "https://graph.microsoft.com/beta/groups?`$filter=resourceProvisioningOptions/Any(x:x eq 'Team')&`$top=999"
+$uri = "https://graph.microsoft.com/beta/teams"
 do {
     $result = Invoke-WebRequest -Headers $AuthHeader1 -Uri $uri -ErrorAction Stop
     $uri = $result.'@odata.nextLink'
@@ -29,9 +34,9 @@ do {
     Start-Sleep -Milliseconds 500
     $Teams += ($result.Content | ConvertFrom-Json).Value
 } while ($uri)
+if (!$Teams -or $Teams.Count -eq 0) { Write-Host "Unable to obtain the list of teams, exiting..."; return }
 
-
-
+#Iterate over each Team and prepare the report
 $ReportApps = @();$ReportTabs = @();$count = 1
 foreach ($team in $Teams) {
 
@@ -43,10 +48,10 @@ foreach ($team in $Teams) {
     $count++
 
     #Simple anti-throttling control
-    Start-Sleep -Milliseconds 200
+    Start-Sleep -Milliseconds 500
 
     #get a list of apps for the Team
-    $teamApps = Invoke-WebRequest -Headers $authHeader1 -Uri "https://graph.microsoft.com/v1.0/teams/$($Team.id)/installedApps?`$expand=teamsApp" -ErrorAction Stop
+    $teamApps = Invoke-WebRequest -Headers $authHeader1 -Uri "https://graph.microsoft.com/beta/teams/$($Team.id)/installedApps?`$expand=teamsApp,teamsAppDefinition" -ErrorAction Stop
     $teamApps = ($teamApps.content | ConvertFrom-Json).Value
 
     $i = 0
@@ -56,15 +61,21 @@ foreach ($team in $Teams) {
         $objApp | Add-Member -MemberType NoteProperty -Name "Number" -Value $i
         $objApp | Add-Member -MemberType NoteProperty -Name "Team" -Value $team.displayName
         $objApp | Add-Member -MemberType NoteProperty -Name "Name" -Value $app.teamsApp.displayName
+        $objApp | Add-Member -MemberType NoteProperty -Name "Version" -Value $app.teamsAppDefinition.version
         $objApp | Add-Member -MemberType NoteProperty -Name "AppId" -Value $app.teamsApp.Id
         $objApp | Add-Member -MemberType NoteProperty -Name "AddedVia" -Value $app.teamsApp.distributionMethod
+        $objApp | Add-Member -MemberType NoteProperty -Name "Description" -Value $app.teamsAppDefinition.description
+        $objApp | Add-Member -MemberType NoteProperty -Name "AADAppID" -Value $app.teamsAppDefinition.azureADAppId
+        $objApp | Add-Member -MemberType NoteProperty -Name "AvailableFor" -Value $app.teamsAppDefinition.allowedInstallationScopes
 
         $ReportApps += $objApp
     }
 
+    #Get a list of channels so we can also cover Tabs
     $TeamChannels = Invoke-WebRequest -Headers $AuthHeader1 -Uri "https://graph.microsoft.com/beta/Teams/$($Team.id)/channels" -ErrorAction Stop
     $TeamChannels = ($TeamChannels.Content | ConvertFrom-Json).value
     
+    #Iterate over each channel, enumerate Tabs
     foreach ($channel in $TeamChannels) {
         $tabs = Invoke-WebRequest -Headers $authHeader1 -Uri "https://graph.microsoft.com/beta/teams/$($Team.id)/channels/$($channel.id)/tabs?`$expand=teamsApp" -ErrorAction Stop
         $tabs = ($tabs.Content | ConvertFrom-Json).value
@@ -80,10 +91,13 @@ foreach ($team in $Teams) {
             $objTab | Add-Member -MemberType NoteProperty -Name "AppName" -Value $tab.teamsApp.displayName
             $objTab | Add-Member -MemberType NoteProperty -Name "AppId" -Value $tab.teamsApp.Id
             $objTab | Add-Member -MemberType NoteProperty -Name "AddedVia" -Value $tab.teamsApp.distributionMethod
+            $objTab | Add-Member -MemberType NoteProperty -Name "DateAdded" -Value $tab.configuration.dateAdded
 
             $ReportTabs += $objTab
         }
     }
 }
+
+#Export the result
 $ReportApps | select * -ExcludeProperty Number | Export-Csv -Path "$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss'))_TeamsAppsReport.csv" -NoTypeInformation -Encoding UTF8 -UseCulture
 $ReportTabs | select * -ExcludeProperty Number | Export-Csv -Path "$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss'))_TeamsTabsReport.csv" -NoTypeInformation -Encoding UTF8 -UseCulture
