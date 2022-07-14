a1cb9c53ef960171394b8e52a8e13d14d458e602
Vasil Michev <38553333+michevnew@users.noreply.github.com>
2022-07-14 09:17:43 +0300

    Add files via upload

---
+++ AADRolesInventory-Graph.ps1
@@ -0,0 +1,108 @@
+﻿#Requires -Version 3.0
+[CmdletBinding(SupportsShouldProcess)] #Make sure we can use -WhatIf and -Verbose
+Param([switch]$IncludePIMEligibleAssignments) #Indicate whether to include PIM elibigle role assignments in the output. NOTE: Currently the RoleManagement.Read.Directory application permissions seems to be required!
+
+#For details on what the script does and how to run it, check: https://www.michev.info/Blog/Post/3958/
+
+#region Authentication
+#We use the client credentials flow as an example. For production use, REPLACE the code below wiht your preferred auth method. NEVER STORE CREDENTIALS IN PLAIN TEXT!!!
+
+#Variables to configure
+$tenantID = "tenant.onmicrosoft.com" #your tenantID or tenant root domain
+$appID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" #the GUID of your app. For best result, use app with Directory.Read.All scope granted. For PIM use RoleManagement.Read.Directory
+$client_secret = "verylongsecurestring" #client secret for the app
+
+#Prepare token request
+$url = 'https://login.microsoftonline.com/' + $tenantId + '/oauth2/v2.0/token'
+
+$body = @{
+    grant_type = "client_credentials"
+    client_id = $appID
+    client_secret = $client_secret
+    scope = "https://graph.microsoft.com/.default"
+}
+
+#Obtain the token
+Write-Verbose "Authenticating..."
+try { $tokenRequest = Invoke-WebRequest -Method Post -Uri $url -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing -ErrorAction Stop }
+catch { Write-Host "Unable to obtain access token, aborting..."; return }
+
+$token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+
+$authHeader = @{
+   'Content-Type'='application\json'
+   'Authorization'="Bearer $token"
+}
+#endregion Authentication
+
+#region Roles
+Write-Verbose "Collecting role assignments..."
+#Use the /roleManagement/directory/roleAssignments endpoint to collect a list of all role assignments.
+$roles = @()
+$uri = 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments?$expand=principal' #$expand=* is BROKEN
+
+do {
+    $result = Invoke-WebRequest -Uri $uri -Verbose:$VerbosePreference -ErrorAction Stop -Headers $authHeader
+    $uri = $($result | ConvertFrom-Json).'@odata.nextLink'
+    #If we are getting multiple pages, best add some delay to avoid throttling
+    Start-Sleep -Milliseconds 500
+    $roles += ($result | ConvertFrom-Json).Value
+} while ($uri)
+
+#fix to also fetch the roleDefinition
+$uri = 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments?$expand=roleDefinition' #$expand=* is BROKEN
+
+$roles1 = @()
+do {
+    $result = Invoke-WebRequest -Uri $uri -Verbose:$VerbosePreference -ErrorAction Stop -Headers $authHeader
+    $uri = $($result | ConvertFrom-Json).'@odata.nextLink'
+    #If we are getting multiple pages, best add some delay to avoid throttling
+    Start-Sleep -Milliseconds 500
+    $roles1 += ($result | ConvertFrom-Json).Value
+} while ($uri)
+
+foreach ($role in $roles) { Add-Member -InputObject $role -MemberType NoteProperty -Name roleDefinition -Value ($roles1 | ? {$_.id -eq $role.id}).roleDefinition }
+
+#process PIM eligible role assignments
+if ($IncludePIMEligibleAssignments) {
+    Write-Verbose "Collecting PIM eligible role assignments..."
+    $uri = 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleEligibilitySchedules?$select=id,principalId,directoryScopeId,roleDefinitionId,status&$expand=*'
+
+    do {
+        $result = Invoke-WebRequest -Uri $uri -Verbose:$VerbosePreference -ErrorAction Stop -Headers $authHeader
+        $uri = $($result | ConvertFrom-Json).'@odata.nextLink'
+        #If we are getting multiple pages, best add some delay to avoid throttling
+        Start-Sleep -Milliseconds 500
+        $roles += ($result | ConvertFrom-Json).Value
+    } while ($uri)
+}
+
+if (!$roles) { Write-Verbose "No valid role assignments found, verify the required permissions have been granted?"}
+
+Write-Verbose "A total of $($roles.count) role assignments were found, of which $(($roles | ? {$_.directoryScopeId -eq "/"}).Count) are tenant-wide and $(($roles | ? {$_.directoryScopeId -ne "/"}).Count) are AU-scoped. $(($roles | ? {!$_.status}).Count) roles are permanently assigned, you might want to address that!" 
+#endregion Roles
+
+#region Output
+Write-Verbose "Preparing the output..."
+$report = @()
+foreach ($role in $roles) {
+    $reportLine=[ordered]@{
+        "Principal" = switch ($role.principal.'@odata.type') {
+            '#microsoft.graph.user' {$role.principal.userPrincipalName}
+            '#microsoft.graph.servicePrincipal' {$role.principalId}
+            '#microsoft.graph.group' {$role.principal.id}
+        }
+        "PrincipalDisplayName" = $role.principal.displayName
+        "PrincipalType" = $role.principal.'@odata.type'.Split(".")[-1]
+        "AssignedRole" = $role.roleDefinition.displayName
+        "AssignedRoleScope" = $role.directoryScopeId
+        "AssignmentType" = (&{if ($role.status -eq "Provisioned") {"Eligible"} else {"Permanent"}})
+        "IsBuiltIn" = $role.roleDefinition.isBuiltIn
+        "RoleTemplate" = $role.roleDefinition.templateId
+    }
+    $report += @([pscustomobject]$reportLine)
+}
+#endregion Output
+
+#format and export
+$report | sort DisplayName | Export-CSV -nti -Path "$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss'))_AzureADRoleInventory.csv"
---
+++ AADRolesInventory-MG.ps1
@@ -0,0 +1,57 @@
+﻿#Requires -Version 3.0
+[CmdletBinding(SupportsShouldProcess)] #Make sure we can use -WhatIf and -Verbose
+Param([switch]$IncludePIMEligibleAssignments) #Indicate whether to include PIM elibigle role assignments in the output. NOTE: Currently the RoleManagement.Read.Directory scope seems to be required!
+
+#For details on what the script does and how to run it, check: https://www.michev.info/Blog/Post/3958/
+
+#region Authentication
+try { Connect-MgGraph -Scopes RoleManagement.Read.Directory,Directory.Read.All} #RoleManagement.Read.Directory needed for PIM eligible assignments, Directory.Read.All for roles and "translating" GUIDs
+catch { Write-Error $_ -ErrorAction Stop; return }
+#endregion Authentication
+
+if (!(Get-MgContext).Scopes.Contains("Directory.Read.All")) { Write-Error "The required permissions are missing, please re-consent!" -ErrorAction Stop }
+
+#region Roles
+Write-Verbose "Collecting role assignments..."
+#Use the Get-MgRoleManagementDirectoryRoleAssignment cmdlet to collect a list of all role assignments.
+$roles = Get-MgRoleManagementDirectoryRoleAssignment -All -ExpandProperty Principal #$expand=* is BROKEN
+$roles1 = Get-MgRoleManagementDirectoryRoleAssignment -All -ExpandProperty roleDefinition #fix to also fetch the roleDefinition
+foreach ($role in $roles) { Add-Member -InputObject $role -MemberType NoteProperty -Name roleDefinition1 -Value ($roles1 | ? {$_.id -eq $role.id}).roleDefinition } #and another fix needed as PowerShell populates empty roleDefinition property...
+
+#Use the Get-MgRoleManagementDirectoryRoleEligibilitySchedule cmdlet to collect a list of all PIM eligible role assignments.
+if ($IncludePIMEligibleAssignments) {
+    Write-Verbose "Collecting PIM eligible role assignments..."
+
+    $roles += (Get-MgRoleManagementDirectoryRoleEligibilitySchedule -All -ExpandProperty * | select id,principalId,directoryScopeId,roleDefinitionId,status,principal,@{n="roleDefinition1";e={$_.roleDefinition}})
+}
+
+if (!$roles) { Write-Verbose "No valid role assignments found, verify the required permissions have been granted?"}
+
+Write-Verbose "A total of $($roles.count) role assignments were found, of which $(($roles | ? {$_.directoryScopeId -eq "/"}).Count) are tenant-wide and $(($roles | ? {$_.directoryScopeId -ne "/"}).Count) are AU-scoped. $(($roles | ? {!$_.status}).Count) roles are permanently assigned, you might want to address that!" 
+#endregion Roles
+
+#region Output
+#prepare the script output
+Write-Verbose "Preparing the output..."
+$report = @()
+foreach ($role in $roles) {
+    $reportLine=[ordered]@{
+        "Principal" = switch ($role.principal.AdditionalProperties.'@odata.type') {
+            '#microsoft.graph.user' {$role.principal.AdditionalProperties.userPrincipalName}
+            '#microsoft.graph.servicePrincipal' {$role.principal.AdditionalProperties.appId}
+            '#microsoft.graph.group' {$role.principalid}
+        }
+        "PrincipalDisplayName" = $role.principal.AdditionalProperties.displayName
+        "PrincipalType" = $role.principal.AdditionalProperties.'@odata.type'.Split(".")[-1]
+        "AssignedRole" = $role.roleDefinition1.displayName
+        "AssignedRoleScope" = $role.directoryScopeId
+        "AssignmentType" = (&{if ($role.status -eq "Provisioned") {"Eligible"} else {"Permanent"}})
+        "IsBuiltIn" = $role.roleDefinition1.isBuiltIn
+        "RoleTemplate" = $role.roleDefinition1.templateId
+    }
+    $report += @([pscustomobject]$reportLine)
+}
+#endregion Output
+
+#format and export
+$report | sort DisplayName | Export-CSV -nti -Path "$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss'))_AzureADRoleInventory.csv"
